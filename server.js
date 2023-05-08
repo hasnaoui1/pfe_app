4291d0f899008a6c9e55646e7c7990ab0c1946b6
hasnaoui1 <108799947+hasnaoui1@users.noreply.github.com>
2023-05-08 18:02:51 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = 3000;
 const HOST = '0.0.0.0';
 
 // Connect to the database
-mongoose.connect('mongodb://mongo:27017/users', {
+mongoose.connect('mongodb://192.168.1.23:27017/users', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
