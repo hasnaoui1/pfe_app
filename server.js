4f6dc0325f2f2e724efaf2eeef6bc48b56e013ca
hasnaoui1 <hasamine66@gmail.com>
2023-05-22 14:05:18 +0100

    changes to server.js

--- server.js
+++ server.js
@@ -51,24 +51,6 @@ app.get('/', (req, res) => {
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/users', async (req, res, next) => {
-  try {
-    const users = await User.find();
-    res.json(users);
-  } catch (error) {
-    next(error);
-  }
-});
-
-app.post('/users', async (req, res, next) => {
-  try {
-    const user = new User(req.body);
-    const result = await user.save();
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
-});
 
 
 // Serve static files from the 'public' directory
@@ -79,6 +61,35 @@ app.get('/create-user', (req, res) => {
   res.sendFile('create-user.html', { root: __dirname + '/' });
 });
 
+app.route('/users')
+  .get(async (req, res, next) => {
+    try {
+      const users = await User.find();
+      let html = '<html><body>';
+      html += '<table>';
+      html += '<tr><th>Name</th><th>Email</th></tr>';
+      users.forEach((user) => {
+        html += '<tr>';
+        html += `<td>${user.name}</td>`;
+        html += `<td>${user.email}</td>`;
+        html += '</tr>';
+      });
+      html += '</table>';
+      html += '</body></html>';
+      res.send(html);
+    } catch (error) {
+      next(error);
+    }
+  })
+  .post(async (req, res, next) => {
+    try {
+      const user = new User(req.body);
+      const result = await user.save();
+      res.redirect('/users'); // Redirect to the /users route after creating the user
+    } catch (error) {
+      next(error);
+    }
+  });
 
 
 
