7c0c2ef26e76e13e44bfa58399f338d05eed10b4
hasnaoui1 <hasamine66@gmail.com>
2023-05-22 14:09:54 +0100

    last to changes to server.js

--- server.js
+++ server.js
@@ -3,13 +3,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-
-
-// Constants
 const PORT = 3000;
 const HOST = '0.0.0.0';
 
-// Connect to the database
 mongoose.connect('mongodb://192.168.1.23:30000/users', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -22,7 +18,6 @@ mongoose.connect('mongodb://192.168.1.23:30000/users', {
   process.exit(1);
 });
 
-// Define a schema for a User
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -35,16 +30,12 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-// Create a model for the User schema
 const User = mongoose.model('User', userSchema);
 
-// App
 const app = express();
-
-// Middleware to parse JSON bodies
 app.use(express.json());
 
-// Route to test the server
+
 app.get('/', (req, res) => {
   res.send('Server is up and running!');
 });
@@ -52,11 +43,8 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-
-// Serve static files from the 'public' directory
 app.use(express.static('public'));
 
-// Route to serve the HTML form
 app.get('/create-user', (req, res) => {
   res.sendFile('create-user.html', { root: __dirname + '/' });
 });
@@ -90,9 +78,6 @@ app.route('/users')
       next(error);
     }
   });
-
-
-
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
